c0963908a1230d8d2d37cb1efecb42c6bc8fe9e6
Shaye Ellerbe <slellerbe23@gmail.com>
2023-02-07 15:26:53 -0500

    First commit

---
+++ notes-app/app.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+//Require function, how we load in modules
+// fs.writeFileSync('notes.txt', 'My name is Shaye.')
+fs.appendFileSync('notes.txt', ' I live in North Carolina.')
+
+
+
+
+//write file and writefile sync methods, are responsible for writing data to a file.
