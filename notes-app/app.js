73da2f4caf7377f698634899e69765be1afdf160
Shaye Ellerbe <slellerbe23@gmail.com>
2023-02-08 23:58:15 -0500

    updates, continue learning

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
+// You need to use the require function to use the packets
 const chalk = require('chalk')
 const yargs = require('yargs')
 const notes = require('./notes.js')
@@ -11,14 +12,14 @@ yargs.command({
     describe: 'Add a new note',
     builder: {
         title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
+            describe: 'Note title', //Allows us to describe that option
+            demandOption: true, //To make title required
+            type: 'string'//Making sure that the value is always a string for title
         },
         body: {
             describe: 'Note body',
-            demandOption: true,
-            type: 'string'
+            demandOption: true, //To make body be required
+            type: 'string' //Making sure that the value is always a string for body
         }
     },
     handler: function (argv) {
@@ -53,4 +54,4 @@ yargs.command({
     }
 })
 
-yargs.parse()
+yargs.parse() //Goes through the process of parsing the arguments with all of the configuration details you've provided with your yargs command calls up above.
